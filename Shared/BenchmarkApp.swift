fdcbace84ce92ba530795968266e5922bbf6d7a0
Gil Biren <gilbiren@gmail.com>
2020-11-25 13:19:26 +0100

    Initial Commit

---
+++ Shared/BenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchmarkApp.swift
+//  Shared
+//
+//  Created by Gil Biren on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct BenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
