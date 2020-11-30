85dbf249312dfa526dad9e6340198c19b621136c
Gil Biren <gilbiren@gmail.com>
2020-11-30 14:47:37 +0100

    Use long string to decrypt

--- Shared/Source/Benchmark/BenchmarkCalculationAES.swift
+++ Shared/Source/Benchmark/BenchmarkCalculationAES.swift
@@ -11,7 +11,7 @@ import CryptoKit
 
 final class BenchmarkCalculationAES: BenchmarkService {
     private let key = SymmetricKey(size: .bits256)
-    private let messageData = "This is a very secure Message 🧐".data(using: .utf8)!
+    private let messageData = String.random(length: 5000).data(using: .utf8)!
     
     override func calculate() {
         if decryptEncrypt() {
@@ -23,5 +23,12 @@ final class BenchmarkCalculationAES: BenchmarkService {
         guard let sealBox = try? AES.GCM.seal(messageData, using: key) else { return false }
         guard let messageData = try? AES.GCM.open(sealBox, using: key) else { return false }
         return self.messageData == messageData
+    }
+}
+
+private extension String {
+    static func random(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.;-£¶¨èà$üö§¢+*ç%&/()=🤯🙈🥸 "
+      return String((0..<length).map{ _ in letters.randomElement()! })
     }
 }
